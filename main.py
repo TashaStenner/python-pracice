a83f82cedf25be9635aeb7d512c3c3e99a3f130f
Natasha Emily Stenner <tashas1997@gmail.com>
2020-07-18 16:45:34 +0000

    challenge 9 complete

--- main.py
+++ main.py
@@ -88,37 +88,57 @@
 
 # challenge 8
    
-print('''Please pick one:
-            rock
-            scissors
-            paper''')
+# print('''Please pick one:
+#             rock
+#             scissors
+#             paper''')
 
-while True:
-    game_dict = {'rock': 1, 'scissors': 2, 'paper': 3}
-    player_a = str(input("Player a: "))
-    player_b = str(input("Player b: "))
-    a = game_dict.get(player_a)
-    b = game_dict.get(player_b)
-    dif = a - b
+# while True:
+#     game_dict = {'rock': 1, 'scissors': 2, 'paper': 3}
+#     player_a = str(input("Player a: "))
+#     player_b = str(input("Player b: "))
+#     a = game_dict.get(player_a)
+#     b = game_dict.get(player_b)
+#     dif = a - b
 
-    if dif in [-1, 2]:
-        print('player a wins.')
-        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
-            continue
-        else:
-            print('game over.')
-            break
-    elif dif in [-2, 1]:
-        print('player b wins.')
-        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
-            continue
-        else:
-            print('game over.')
-            break
+#     if dif in [-1, 2]:
+#         print('player a wins.')
+#         if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+#             continue
+#         else:
+#             print('game over.')
+#             break
+#     elif dif in [-2, 1]:
+#         print('player b wins.')
+#         if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+#             continue
+#         else:
+#             print('game over.')
+#             break
+#     else:
+#         print('Draw.Please continue.')
+#         print('')
+
+#challenge 9
+
+import random
+number = random.randint(1,9)
+guess = 0 
+
+while guess != number and guess != "exit":
+  guess = input("Take a guess between 1 and 10")
+
+  if guess == "exit":
+    break
+      
+    guess = int(guess)
+    
+    if guess < number:
+        print("Bit low!")
+    elif guess > number:
+        print("Bit high!")
     else:
-        print('Draw.Please continue.')
-        print('')
+        print("You got it!")
 
 
-
-
+    
